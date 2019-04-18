0e8f3fa9e25ba6b030662de2692409757c1770a4
Farjana Huq <fa.huq03@gmail.com>
2019-04-17 17:32:11 -0700

    sendgrid email works

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,17 +4,17 @@ const db = require('../models');
 const crypto = require('crypto');
 const helpers = require('./helpers/authHelpers');
 const jwt = require('jsonwebtoken');
-//var sgMail = require('@sendgrid/mail');
-var nodemailer = require('nodemailer');
-var sgTransport = require('nodemailer-sendgrid-transport');
+var sgMail = require('@sendgrid/mail');
+// var nodemailer = require('nodemailer');
+// var sgTransport = require('nodemailer-sendgrid-transport');
 
-var options = {
-   auth: {
-     api_user: 'farjanaHuq13',
-     api_key: 'mySendgrid2019'
-   }
- }
- var client = nodemailer.createTransport(sgTransport(options));
+// var options = {
+//    auth: {
+//      api_user: process.env.Nodemailer_API_USER,
+//      api_key: process.env.Nodemailer_API_KEY
+//    }
+//  }
+//  var client = nodemailer.createTransport(sgTransport(options));
 // register
 router.post('/register', (req, res) => {
    if (!req.body.displayName || !req.body.password || !req.body.email) {
@@ -32,45 +32,29 @@ router.post('/register', (req, res) => {
 
    user.hash = helpers.getHash(user.salt, req.body.password);
    // sendgrid
-   //sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-   // check for environment to send proper links in email
-  // var urlInEmail = 'http://localhost:3000/';
-   // if (!process.env.NODE_ENV) urlInEmail = 'http://localhost:3000/';
-   // else urlInEmail = 'https://desolate-cliffs-99613.herokuapp.com/';
-   // const msg = {
+   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+ 
+
+   // var email = {
+   //    from: 'noreply@jumpingJalapinoRabbit.com',
    //    to: 'huq.farjana03@gmail.com',
-   //    from: 'noreply@followthemoneytrail.org',
-   //    subject: 'Verify Email Address',
-   //    text: 'Verify Email Address',
-   //    html: `
-   //          <p>An account has just been created with this email address at followthemoneytrail.org.
-   //          <p>To activate your new account, please click the following link:</p>`
-   //          // <a href="${urlInEmail}VerifyEmail?k=${req.body.validationKey}">${urlInEmail}VerifyEmail?k=${req.body.validationKey}</a>
-   //       ,
-   // };
-   // sgMail.send(msg);
-
-
-   var email = {
-      from: 'noreply@jumpingJalapinoRabbit.com',
-      to: 'huq.farjana03@gmail.com',
-      subject: 'Email Activation',
-      text: 'Click the link to activate your email',
-      // <p>To confirm your registration, please click the following link:</p>
-      // <a href="http://localhost:3000/activate/${emailToken}">http://localhost:3000/activate</a>`,
-      html: '<strong><b>Hello</b></strong>'
-      // <p>To confirm your registration, please click the following link:</p>
-      // <a href="http://localhost:3000/activate/${emailToken}">http://localhost:3000/activate</a>`
-    };
+   //    subject: 'Email Activation',
+   //    text: 'Click the link to activate your email',
+   //    // <p>To confirm your registration, please click the following link:</p>
+   //    // <a href="http://localhost:3000/activate/${emailToken}">http://localhost:3000/activate</a>`,
+   //    html: '<strong><b>Hello</b></strong>'
+   //    // <p>To confirm your registration, please click the following link:</p>
+   //    // <a href="http://localhost:3000/activate/${emailToken}">http://localhost:3000/activate</a>`
+   //  };
     
-    client.sendMail(email, function(err, info){
-        if (err ){
-          console.log(err);
-        }
-        else {
-          console.log('Message sent: ' + info.response);
-        }
-    });
+   //  client.sendMail(email, function(err, info){
+   //      if (err ){
+   //        console.log(err);
+   //      }
+   //      else {
+   //        console.log('Message sent: ' + info.response);
+   //      }
+   //  });
    
    db.User.create(user)
       .then(resp => {
@@ -80,7 +64,16 @@ router.post('/register', (req, res) => {
       })
          
       .catch(err => res.status(400).json({ msg: err.toString() }));
-
+    
+      sgMail.setApiKey(process.env.REACT_APP_SENDGRID_API_KEY);
+      const msg = {
+        to: 'huq.farjana03@gmail.com',
+        from: 'test@example.com',
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+      };
+      sgMail.send(msg);
      
 });
 
